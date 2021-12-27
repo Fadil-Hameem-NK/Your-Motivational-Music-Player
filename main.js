7842b91baa5701a73a212b0c59bcc727899e00a8
Fadil-Hameem-NK <95924263+Fadil-Hameem-NK@users.noreply.github.com>
2021-12-27 16:46:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function PlayCradles() {
+  document.getElementById("Cradlesaudio").play();
+}
+function PlayBeliever() {
+  document.getElementById("Believeraudio").play();
+}
+function PlayAlone() {
+  document.getElementById("Aloneaudio").play();
+}
+function PlayDarkside() {
+  document.getElementById("Darksideaudio").play();
+}
+function PlayAloneNico() {
+  document.getElementById("AloneNicoaudio").play();
+}
+function PlayLegendsNeverDie() {
+  document.getElementById("Legendsneverdieaudio").play();
+}
+
+
+
